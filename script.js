92147d42d45c4c25a10f43d93d3274311be78d0c
Mangesh Bodke <mangeshbodke2025@gmail.com>
2025-08-07 23:37:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+
+let userResponse = confirm("Do you want to proceed?");
+
+if (userResponse) {
+    alert("You clicked OK. Proceeding...");
+} else {
+    alert("You clicked Cancel. Exiting...");
+}
+
+
+let userResponse = confirm("Do you want to proceed?");
+
+if (userResponse) {
+    alert("You clicked OK. Proceeding...");
+} else {
+    alert("You clicked Cancel. Exiting...");
+}
